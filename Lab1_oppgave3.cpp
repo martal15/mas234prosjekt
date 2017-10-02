62cdb65b426744cf5c8699554dc8ecce6aa7a11d
Martine Alvestad <martal15@student.uia.no>
2017-10-02 11:38:26 +0200

    Initial commit

---
+++ Lab1_oppgave3.cpp
@@ -0,0 +1,22 @@
+/*
+ * GccApplication2.cpp
+ *
+ * Created: 02.10.2017 11:16:44
+ * Author : m_alv
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	    /* Replace with your application code */
+	DDRD = 0x01;
+	PORTD = 0x0;
+	
+    while (1) 
+    {
+		
+    }
+}
+
